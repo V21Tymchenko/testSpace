96b36cb9e55fd6b384db0651e93f0fea0a334f72
Valeriia tymchenko <lera.tymchenko@ukr.net>
2024-03-21 13:08:02 +0200

    Update AddFile.tsx

--- src/components/AddFile/AddFile.tsx
+++ src/components/AddFile/AddFile.tsx
@@ -1,12 +1,14 @@
 import { DragEvent, FC, useState } from "react";
 import { FilesForm } from "./AddFile.styled";
 import * as asn1js from "asn1js";
-import CertificateData from '../ CertificateData/ CertificateData';
+import CertificateData from "../ CertificateData/ CertificateData";
+import { ThemeConsumer } from "styled-components";
+import { rejects } from "assert";
 
 const AddFile: FC = () => {
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const [files, setFiles] = useState<File[]>([]);
-   const [certificateData, setCertificateData] = useState<any>(null);
+  const [certificateData, setCertificateData] = useState<any>(null);
 
   const handleDrag = (e: DragEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -47,7 +49,7 @@ const AddFile: FC = () => {
 
           // Розпарсування даних ASN.1 структури
           const parsedData = asn1.result;
-            setCertificateData(parsedData); 
+          setCertificateData(parsedData);
           console.log(parsedData); // Отримані дані з ASN.1 структури
 
           setFiles([file]);
@@ -78,3 +80,39 @@ const AddFile: FC = () => {
 };
 
 export default AddFile;
+
+function maxSumFromTopToBottom(triangle) {
+  const n = triangle.length;
+
+  // Створюємо матрицю dp для зберігання проміжних результатів
+  const dp = new Array(n).fill().map(() => new Array(n).fill(0));
+  console.log("dp", dp);
+
+  //Починаємо з копіювання останнього рядка трикутника у матрицю dp
+  for (let i = 0; i < n; i++) {
+    dp[n - 1][i] = triangle[n - 1][i];
+  }
+
+  // Проходимося по решті рядків трикутника знизу вгору
+  for (let i = n - 2; i >= 0; i--) {
+    for (let j = 0; j <= i; j++) {
+      // Для кожної клітинки dp [i] [j], знаходимо максимум з двох сусідніх клітинок у наступному рядку трикутника і додаємо до поточної клітинки
+      dp[i][j] = triangle[i][j] + Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+    }
+  }
+
+  // dp[0][0] містить максимальну кількість балів під час спуску з гори
+  return dp[0][0];
+}
+
+// Приклад використання
+const triangle = [
+  [6],
+  [7, 10],
+  [12, 11, 9],
+  [90, 25, 13, 14],
+  [1, 113, 118, 2, 4],
+];
+const result = maxSumFromTopToBottom(triangle);
+console.log(result);
+
